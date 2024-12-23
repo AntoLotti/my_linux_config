3e42af1166c49e18b2efa438009b28a474bed87f
Antonio <antoniolottivillar@gmail.com>
2024-12-23 11:33:27 +0100

    Feat: modification of the packes script, added a funtion to test

--- setup/setup_packages.sh
+++ setup/setup_packages.sh
@@ -3,127 +3,51 @@
 # Terminal Colors
 GREEN='\033[0;32m'
 RED='\033[0;31m'
-ORGANGE=''
+BLUE='\033[0;34m'
 NC='\033[0m' # Sin color
+
+# ====== Functions ======
+install_package() {
+    local package_name=$1
+    echo -e "${BLUE}========== Installing $package_name ===========${NC}"
+    sudo apt-get install -y $package_name
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}Error installing $package_name. Aborting.${NC}"
+        exit 1
+    fi
+}
 
 # ====== Start Of Script ======
 echo -e "${GREEN}===================== INSTALLING PACKAGES ================================${NC}"
 
-# ====== Packages Managers ====
-echo -e "${GREEN}========== Installing Snap =============${NC}"
-sudo apt-get install snapd -y
+# PACKAGES MANAGERS #
+## snap
+install_package snapd
+## synaptic
+install_package synaptic
+## faltpak
+install_package flatpak
+## wget
+install_package wget
+## curl
+install_package curl
 
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
+# BASIC PACKAGES #
+install_package build-essential
+install_package software-properties-common
+install_package "unzip zip tar gzip"
+install_package usbutils
+install_package udisks2
+install_package dosfstools
+install_package exfat-utils
+install_package ntfs-3g
 
-echo -e "${GREEN}========== Installing Synaptic =========${NC}"
-sudo apt install synaptic -y
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
-
-echo -e "${GREEN}========== Installing Flatpak ==========${NC}"
-sudo apt-get install flatpak -y
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
-
-echo -e "${GREEN}========== Installing Curl and wget ======${NC}"
-sudo apt install wget curl -y
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
-
-# ====== Basics Packages ======
-
-sudo apt-get install build-essential -y
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
-
-sudo apt-get install software-properties-common -y
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
-
-sudo apt-get install unzip zip tar gzip -y
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
-
-sudo apt install usbutils udisks2
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
-
-sudo apt install dosfstools exfat-utils
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
-
-sudo apt install ntfs-3g
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
-
-# ====== Python ====================
-sudo apt install python3-full -y
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
-
-echo -e "${GREEN}========== Installing Python ===========${NC}"
-sudo apt install python3 -y
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
-
-echo -e "${GREEN}========== Installing pip ==============${NC}"
-sudo apt install python3-pip -y
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
-
-echo -e "${GREEN}========== Installing Python3-venv =====${NC}"
-sudo apt install python3-venv -y
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
-
-sudo apt -y install python3-launchpadlib
-
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Error al instalar dependencias. Aborta.${NC}"
-    exit 1
-fi
+# PYTHON #
+install_package python3-full
+install_package python3
+install_package python3-pip
+install_package python3-venv
+install_package python3-launchpadlib
 
 # ====== End Of Script ======
 echo -e "${GREEN}===================== END OF PACKAGES ====================================${NC}"
