48b21f4ae3369046569a28b6ca8c6c8e4b02be69
Antonio <antoniolottivillar@gmail.com>
2025-01-03 12:15:11 +0100

    CHANGED: put Browser into the environment file and removed of the apps file

--- setup/setup_apps.sh
+++ setup/setup_apps.sh
@@ -17,17 +17,10 @@ install_package() {
     fi
 }
 
-# ====== Repositories =========
-
-## Brave
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
 
 # ====== Start Of Script ======
 echo -e "${GREEN}===================== INSTALLING APPS ====================================${NC}"
 
-install_package brave-browser
 install_package libreoffice
 install_package spotify 
 
--- setup/setup_environment.sh
+++ setup/setup_environment.sh
@@ -17,6 +17,12 @@ install_package() {
     fi
 }
 
+# ====== Repositories =========
+
+## Brave
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
 # ====== Start Of Script ======
 echo -e "${GREEN}===================== INSTALLING ENVIRONMENT =============================${NC}"
 
@@ -59,6 +65,8 @@ install_package picom
 # NOTIFICATION GESTOR
 install_package dunst
 
+# BROWSER
+install_package brave-browser
 
 ## TERMINAL ENVIRONMENT ##
 # ----------------------- #
