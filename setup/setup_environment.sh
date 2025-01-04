1153c63b79d5cff1d07ab6baad43aa47846ee36a
Antonio <antoniolottivillar@gmail.com>
2025-01-04 15:36:28 +0100

    FIXED: instalation of brave-browser

--- setup/setup_environment.sh
+++ setup/setup_environment.sh
@@ -106,6 +106,9 @@ resumen_fn() {
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
+## Update
+sudo apt update
+
 # ====== Start Of Script ======
 echo -e "${GREEN}===================== INSTALLING ENVIRONMENT =============================${NC}"
 
