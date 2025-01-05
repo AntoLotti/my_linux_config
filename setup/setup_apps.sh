0a22d61b54b9f49ee60ced4a4b4d5189e6ea5685
Antonio <antoniolottivillar@gmail.com>
2025-01-05 19:00:24 +0100

    FIXED: Added Spotify repository to install it

--- setup/setup_apps.sh
+++ setup/setup_apps.sh
@@ -100,6 +100,14 @@ resumen_fn() {
     fi
 }
 
+# ====== Repositories =========
+
+## Spotify
+url -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+## Update
+sudo apt update
 
 # ====== Start Of Script ======
 echo -e "${GREEN}===================== INSTALLING APPS ====================================${NC}"
